ad30525843c98fa966c450476f46aa06c38b1042
Qiki <qi@brandingbrand.com>
2015-01-11 14:42:03 -0500

    Initial Commit

---
+++ WeightTracking/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeightTracking
+//
+//  Created by kiki on 1/11/15.
+//  Copyright (c) 2015 kiki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
